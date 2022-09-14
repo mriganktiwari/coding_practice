8cc8040362935d8f39934a7320fc58f37f5eb84b
mriganktiwari <tiwarimrigank2@gmail.com>
2022-09-14 21:58:39 +0530

    agree code with 2 conditions in a conditional

--- cs50_work/lecture-1/agree.c
+++ cs50_work/lecture-1/agree.c
@@ -6,11 +6,12 @@ int main(void)
     char c = get_char("Do you agree ? ");
     
     // Single quotes mean character; Double quotes mean more than 1 character
-    if (c == 'y')
+    // || means the OR operator on conditionals
+    if (c == 'y' || c == 'Y')
     {
         printf("Agreed.\n");
     }
-    else if (c == 'n')
+    else if (c == 'n' || c == 'N')
     {
         printf("Not agreed.\n");
     }
