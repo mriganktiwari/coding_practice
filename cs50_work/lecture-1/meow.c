8535df5b1d5f830a08e0b67a6e7191e01743c249
mriganktiwari <tiwarimrigank2@gmail.com>
2022-09-14 22:23:30 +0530

    added printing meow 3 times with a while loop

--- cs50_work/lecture-1/meow.c
+++ cs50_work/lecture-1/meow.c
@@ -2,7 +2,11 @@
 
 int main(void)
 {
-    printf("meow\n");
-    printf("meow\n");
-    printf("meow\n");
+    int i = 0;
+    while (i < 3)
+    {
+        printf("meow\n");
+        i++;
+    }
+    
 }
