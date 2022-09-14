93527aa2c56d7624da2f73fced132612fe9a1cfc
mriganktiwari <tiwarimrigank2@gmail.com>
2022-09-14 21:40:17 +0530

    adding compare code with conditionals

---
+++ cs50_work/lecture-1/compare.c
@@ -0,0 +1,23 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int main(void)
+{
+    int x = get_int("What is x ?\n");
+    int y = get_int("What is y ?\n");
+
+    if (x<y)
+    {
+        printf("x is less than y \n");
+    }
+
+    else if (x>y)
+    {
+        printf("x is greater than y \n");
+    }
+
+    else
+    {
+        printf("x is equal to y \n");
+    }
+}
