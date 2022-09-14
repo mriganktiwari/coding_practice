e8a35ff28a9011de9030799d6143817f6b84782c
mriganktiwari <tiwarimrigank2@gmail.com>
2022-09-14 22:49:18 +0530

    adding code to make walls like in Mario

---
+++ cs50_work/lecture-1/mario.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+// int main(void)
+// {
+//     for (int i=0; i<4; i++)
+//     {
+//         printf("?");
+//     }
+//     printf("\n");
+// }
+
+// int main(void)
+// {
+//     for (int i=0; i<3; i++)
+//     {
+//         printf("#\n");
+//     }
+// }
+
+int main(void)
+{
+    for (int i=0; i<3; i++)
+    {
+        for (int j=0; j<3; j++)
+        {
+            printf("#");
+        }
+        printf("\n");
+    }
+}
