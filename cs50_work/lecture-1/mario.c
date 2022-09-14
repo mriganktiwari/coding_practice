3d804ae1748a88a942925a1d789a8274901b50db
mriganktiwari <tiwarimrigank2@gmail.com>
2022-09-14 22:55:41 +0530

    new feature of declaring a variable - constant

--- cs50_work/lecture-1/mario.c
+++ cs50_work/lecture-1/mario.c
@@ -17,11 +17,24 @@
 //     }
 // }
 
+// int main(void)
+// {
+//     for (int i=0; i<3; i++)
+//     {
+//         for (int j=0; j<3; j++)
+//         {
+//             printf("#");
+//         }
+//         printf("\n");
+//     }
+// }
+
 int main(void)
 {
-    for (int i=0; i<3; i++)
+   const int n = 5; // const will fix n as a constant, no line of code in future could change it
+   for (int i=0; i<n; i++)
     {
-        for (int j=0; j<3; j++)
+        for (int j=0; j<n; j++)
         {
             printf("#");
         }
