be27a68c0cae707e1b29e21e6e3e322efbcc7ccc
mriganktiwari <tiwarimrigank2@gmail.com>
2024-01-20 20:13:39 +0530

    Added multi head attention and FFN

--- makemore/gpt/bigram_v3.py
+++ makemore/gpt/bigram_v3.py
@@ -70,12 +70,32 @@ class Head(nn.Module):
         out = wei @ v # (B,T,T) @ (B,T,head_size) --> (B,T,head_size)
         return out
 
+class MultiHeadAttention(nn.Module):
+    def __init__(self, num_heads, head_size):
+        super().__init__()
+        self.heads = nn.ModuleList([Head(head_size=head_size) for _ in range(num_heads)])
+
+    def forward(self, x):
+        return torch.cat([h(x) for h in self.heads], dim=-1)
+
+class FeedForward(nn.Module):
+    def __init__(self, n_embd):
+        super().__init__()
+        self.net = nn.Sequential(
+            nn.Linear(n_embd, n_embd),
+            nn.ReLU(),
+        )
+
+    def forward(self, x):
+        return self.net(x)
+
 class BigramLanguageModel(nn.Module):
     def __init__(self):
         super().__init__()
         self.token_embedding_table = nn.Embedding(vocab_size, n_embd)
         self.position_embedding_table = nn.Embedding(block_size, n_embd)
-        self.sa_head = Head(head_size=n_embd)
+        self.sa_heads = MultiHeadAttention(num_heads=4, head_size=n_embd//4)
+        self.ffwd = FeedForward(n_embd)
         self.lm_head = nn.Linear(n_embd, vocab_size)
 
     def forward(self, idx, targets=None):
@@ -83,7 +103,8 @@ class BigramLanguageModel(nn.Module):
         tok_emb = self.token_embedding_table(idx) # (B,T,n_embd)
         pos_emb = self.position_embedding_table(torch.arange(T, device=device)) #(T,n_embd)
         x = tok_emb + pos_emb
-        x = self.sa_head(x) # (B,T,head_size)
+        x = self.sa_heads(x) # (B,T,head_size)
+        x = self.ffwd(x)
         logits = self.lm_head(x) # (B,T,C=vocab_size)
         
         if targets is None:
