9dc09f9faafaebcd4f9e34e6e138b0a8d73a4e1e
Mrigank Tiwari <tiwarimrigank2@gmail.com>
2024-10-02 11:44:11 +0530

    single head self-attention object

--- makemore_start_26Aug2024/gpt.py
+++ makemore_start_26Aug2024/gpt.py
@@ -5,12 +5,13 @@ import torch.nn as nn
 # hparams -----------
 batch_size = 32
 block_size = 8
-lr = 1e-2
-max_iters = 3000
-eval_interval = 300
+lr = 1e-3
+max_iters = 5000
+eval_interval = 500
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 eval_iters = 200
 n_embd = 32
+head_size = 12
 # ------------
 torch.manual_seed(1337)
 
@@ -57,11 +58,17 @@ class BigramLM(nn.Module):
     def __init__(self):
         super().__init__()
         self.token_embedding_table = nn.Embedding(vocab_size, n_embd)
+        self.position_embedding_table = nn.Embedding(block_size, n_embd)
+        self.sa_head = Head(head_size=n_embd)
         self.lm_head = nn.Linear(n_embd, vocab_size)
     
     def forward(self, idx, targets=None):
+        B,T = idx.shape
         tok_emb = self.token_embedding_table(idx) # (B,T,n_embd)
-        logits = self.lm_head(tok_emb) # (B,T,vocab_size)
+        pos_emb = self.position_embedding_table(torch.arange(T, device=device)) # (T,n_embd)
+        x = tok_emb + pos_emb # (B,T,n_embd)
+        x = self.sa_head(x) # (B,T,n_embd)
+        logits = self.lm_head(x) # (B,T,vocab_size)
         if targets is None:
             loss = None
         else:
@@ -74,8 +81,10 @@ class BigramLM(nn.Module):
     def generate(self, idx, max_new_tokens):
         # idx is (B,T) array of indices in current context
         for _ in range(max_new_tokens):
+            # crop idx to the last block_size tokens
+            idx_cond = idx[:, -block_size:]
             # getting the predictions
-            logits, loss = self(idx)
+            logits, loss = self(idx_cond)
             # focus on the last time step
             logits = logits[:,-1,:] # becomes (B, C)
             probs = F.softmax(logits, dim=-1) # (B, C)
@@ -83,6 +92,27 @@ class BigramLM(nn.Module):
             idx_nxt = torch.multinomial(probs, num_samples=1) # (B, 1)
             idx = torch.cat([idx, idx_nxt], dim=1) # becomes (B, T+1)
         return idx
+
+class Head(nn.Module):
+    # one head of self-attention
+    def __init__(self, head_size):
+        super().__init__()
+        self.key = nn.Linear(n_embd, head_size, bias=False)
+        self.query = nn.Linear(n_embd, head_size, bias=False)
+        self.value = nn.Linear(n_embd, head_size, bias=False)
+        self.register_buffer('tril', torch.tril(torch.ones(block_size, block_size)))
+
+    def forward(self, x):
+        B,T,C = x.shape
+        k = self.key(x) # (B,T,head_size)
+        q = self.query(x) # (B,T,head_size)
+        # calculating attention scores ('affinities')
+        wei = q @ k.transpose(-2,-1) * head_size**-0.5 # (B,T,head_size) @ # (B,head_size,T) ---> (B, T, T)
+        wei = wei.masked_fill(self.tril[:T,:T]==0, float('-inf')) # (B, T, T)
+        wei = F.softmax(wei, dim=-1) # (B,T,T)
+        v = self.value(x) # (B,T,head_size)
+        out = wei @ v # (B,T,T) @ (B,T,head_size) ---> (B,T,head_size)
+        return out
 
 model = BigramLM()
 m = model.to(device)
